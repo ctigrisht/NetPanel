6794a04e5edd8883ad854f2a086e4d3292553fa2
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-12 12:09:24 +0200

    added model for framework installs

--- NetPanel/Data/HostedApplication.cs
+++ NetPanel/Data/HostedApplication.cs
@@ -10,6 +10,40 @@ using MongoDB.Driver;
 
 namespace NetPanel.Data
 {
+    public class Mock
+    {
+        public Mock()
+        {
+            var dotnet31 = new SystemFramework()
+            {
+                FrameworkName = ".NET Core Runtime",
+                Version = "3.1",
+                Description = "Cross Platform framework to run .NET applications",
+                InstallCommands = new List<string>()
+                {
+                    "sudo wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb",
+                    "sudo dpkg -i packages-microsoft-prod.deb",
+                    "sudo apt-get update",
+                    "sudo apt-get install apt-transport-https",
+                    "sudo apt-get update",
+                    "sudo apt-get install dotnet-sdk-3.1",
+                }
+            };
+        }
+    }
+
+
+    [BsonNoId]
+    public class SystemFramework
+    {
+        public string FrameworkName { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public bool Installed { get; set; }
+
+        public List<string> InstallCommands = new List<string>();
+    }
+
     public class HostedApplication
     {
         [BsonId]
