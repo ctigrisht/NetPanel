22201242068b7912fc1c6327d19c2d93875ebf85
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-13 08:20:08 +0200

    better readme

--- NetPanel/Data/HostedApplication.cs
+++ NetPanel/Data/HostedApplication.cs
@@ -14,21 +14,37 @@ namespace NetPanel.Data
     {
         public Mock()
         {
-            var dotnet31 = new SystemFramework()
+            var frameworks = new List<SystemFramework>()
             {
-                FrameworkName = ".NET Core Runtime",
-                Version = "3.1",
-                Description = "Cross Platform framework to run .NET applications",
-                InstallCommands = new List<string>()
+                new SystemFramework()
                 {
-                    "sudo wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb",
-                    "sudo dpkg -i packages-microsoft-prod.deb",
-                    "sudo apt-get update",
-                    "sudo apt-get install apt-transport-https",
-                    "sudo apt-get update",
-                    "sudo apt-get install dotnet-sdk-3.1",
+                    FrameworkName = ".NET Core Runtime",
+                    Version = "3.1",
+                    Description = "Cross Platform framework to run .NET applications",
+                    Distro = BaseDistro.Debian,
+                    InstallCommands = new List<string>()
+                    {
+                        "sudo wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb",
+                        "sudo dpkg -i packages-microsoft-prod.deb",
+                        "sudo apt-get update",
+                        "sudo apt-get install apt-transport-https",
+                        "sudo apt-get update",
+                        "sudo apt-get install dotnet-sdk-3.1",
+                    }
+                },
+                new SystemFramework()
+                {
+                    FrameworkName = "NodeJS",
+                    Version = "Latest",
+                    Description = "Javascript Interpreter/Web server",
+                    
+                    InstallCommands = new List<string>()
+                    {
+                        "sudo apt install nodejs"
+                    }
                 }
             };
+
         }
     }
 
@@ -40,8 +56,24 @@ namespace NetPanel.Data
         public string Version { get; set; }
         public string Description { get; set; }
         public bool Installed { get; set; }
+        public BaseDistro Distro { get; set; }
 
         public List<string> InstallCommands = new List<string>();
+
+        public void Install()
+        {
+            foreach (var installCommand in InstallCommands)
+            {
+                Lib.ExecuteShell(installCommand);
+            }
+        }
+    }
+
+    public enum BaseDistro  
+    {
+        Debian,
+        Fedora,
+        Arch
     }
 
     public class HostedApplication
--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # NetPanel
 Panel for managing apps hosted on a linux machine
 
-
-
-`Contributors` 
+## Contributors
 All help is appreciated, just message me on discord `flkXI#3462`
+
+## All planned functionalities
+### Hosted Applications Management
+* Choice between binary application startup or script startup
+* Upload and manage files of application directory directly from the interface
+
+### System Management
+* User accounts with custom ranks and permissions, scriptable permissions through ES6
+* Get an APT Repository list of packages to install
+* Execute bash commands, safe or 
+### RCON
+* Run ES6 Scripts through API provided in app (Javascript)
+
