6d0e3548286d33b4f836f5fa16b3f3e797e3b60a
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-13 08:56:10 +0200

    added otp base

---
+++ NetPanel/Data/Users.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NetPanel.Data
+{
+    public class Users
+    {
+        [BsonId][BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPwd { get; set; }
+        public bool EmailOtp { get; set; }
+
+        public int SendOtp()
+        {
+            var otp = new Random().Next(1001, 9999);
+            var mail = new MailMessage("noreply", Email, "NetPanel OTP", $"Your otp is: {otp}");
+
+            return otp;
+        }
+    }
+
+    public enum BasePermissions
+    {
+        God,
+        Admin,
+        Manage,
+        RCON
+    }
+}
