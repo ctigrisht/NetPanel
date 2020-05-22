e47ceb3babee32b52c044f0373958c2841f42933
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-22 14:51:21 +0200

    finished adding major frameworks

--- NetPanel/Data/SystemFramework.cs
+++ NetPanel/Data/SystemFramework.cs
@@ -51,7 +51,7 @@ namespace NetPanel.Data
                         "sudo apt-get update",
                         "sudo apt-get install apt-transport-https",
                         "sudo apt-get update",
-                        "sudo apt-get install dotnet-sdk-3.1",
+                        "sudo apt-get install dotnet-runtime-3.1",
                     }
                 },
                 new SystemFramework
